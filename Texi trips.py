64c5fa0dbb63a3250d694ff9aa065eface26f14d
UwUmeowmeow <98915612+UwUmeowmeow@users.noreply.github.com>
2023-02-20 09:34:58 +1300

    .

--- Texi trips.py
+++ Texi trips.py
@@ -1,18 +1,18 @@
-name = input("What is the driver name: ")
+name = input("What is the driver's name: ")
 
-trip_count =0
+trip_count = 0
 total_time = 0
 total_income = 0
 Base_cost = 10
 
 while True:
     try:
-        trip = int(input("Do you want to start a new trip?\nYes (1)"
-                         "\nNo (2)\nEnter Yes or No: "))
+        trip = input("Do you want to start a new trip?\nYes (1)"
+                     "\nNo (2)\nEnter Yes or No: ").lower()
     except ValueError:
         print("Error please type only Yes or No")
 
-    if trip == 1:
+    if trip == "1" or trip == "yes":
         try:
             trip_time = float(input("How long will it take (IN MINUTES)? "))
             trip_count += 1
@@ -24,7 +24,7 @@ while True:
         except ValueError:
             print("Error please type only numbers")
 
-    elif trip == 2:
+    elif trip == "2" or trip == "no":
 
         print(f"Driver {name} had {trip_count} total trip time of "
               f"{round(total_time)}"
@@ -35,6 +35,9 @@ while True:
               f"Total income for the day was ${total_income:.2f}\n"
               f"The average cost for all trip was "
               f"${total_time / trip_count:.2f}")
+
+    else:
+        print("Please enter a valid choice")
 
 
 
